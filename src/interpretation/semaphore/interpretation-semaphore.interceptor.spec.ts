8da9fc834a2ec335404270b9994fc7e3d0d9ce9d
swlee <swo98@naver.com>
2025-11-21 19:02:52 +0900

    test: seamphore 획득/해제를 lua스크립트로 변경한 테스트 수정

--- src/interpretation/semaphore/interpretation-semaphore.interceptor.spec.ts
+++ src/interpretation/semaphore/interpretation-semaphore.interceptor.spec.ts
@@ -9,29 +9,31 @@ import { InterpretationThrottleException } from "../exceptions/interpretation-th
 describe("InterpretationSemaphoreInterceptor", () => {
   let interceptor: InterpretationSemaphoreInterceptor;
 
-  // redis client 오픈
-  const redisService = { getClient: jest.fn() };
-  const redisClient = {
+  // redis service mocking
+  // 세마포어 획득/해제만 가져옴
+  const redisService = {
     // ioredis의 각 연산 리턴을 jest 러너의 Mock 타입으로 맞춰주기
     // mockResolvedValueOnce 사용에서 요구함
-    incr: jest.fn() as jest.MockedFunction<any>,
-    decr: jest.fn() as jest.MockedFunction<any>,
-    expire: jest.fn() as jest.MockedFunction<any>,
+    acquireSemaphore: jest.fn() as jest.MockedFunction<any>,
+    releaseSemaphore: jest.fn() as jest.MockedFunction<any>,
   };
   const context = {} as ExecutionContext;
 
   /*  
-    // 인터셉터의 다음 실행 mocking
+    인터셉터의 다음 실행 mocking
     intercept(context, next) {
       return next.handle();   // -> Observable
       // controller or 다음 interceptor 으로 가는걸 ok처리
     }
+  
+    intercept(context, next) 내부에서 next.handle()을 호출해야
+    controller 혹은 다음 interceptor 로직이 실행되니까
+    handle()이 호출됐는지만 확인하면 pass 여부를 검증할 수 있음
   */
   const next: CallHandler = { handle: jest.fn(() => of("OK")) };
 
   beforeEach(async () => {
     jest.clearAllMocks();
-    redisService.getClient.mockReturnValue(redisClient);
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -46,23 +48,21 @@ describe("InterpretationSemaphoreInterceptor", () => {
     interceptor = module.get(InterpretationSemaphoreInterceptor);
   });
 
-  it("semaphore 허용량 이내면 pass", async () => {
-    redisClient.incr.mockResolvedValueOnce(1); // 첫번째, ttl설정
-    redisClient.expire.mockResolvedValueOnce(null);
-    redisClient.decr.mockResolvedValueOnce(null);
+  it("세마포어 허용량 이내면 pass", async () => {
+    redisService.acquireSemaphore.mockResolvedValueOnce(true);
 
     await firstValueFrom(interceptor.intercept(context, next));
 
     expect(next.handle).toHaveBeenCalled();
-    expect(redisClient.decr).toHaveBeenCalled();
+    expect(redisService.releaseSemaphore).toHaveBeenCalled();
   });
 
-  it("semaphore 획득 실패하면 InterpretationThrottleException 터짐", async () => {
-    redisClient.incr.mockResolvedValueOnce(999);
-    redisClient.decr.mockResolvedValueOnce(null);
+  it("세마포어 획득 실패 시 InterpretationThrottleException 발생", async () => {
+    redisService.acquireSemaphore.mockResolvedValueOnce(false);
 
     await expect(
       firstValueFrom(interceptor.intercept(context, next))
     ).rejects.toBeInstanceOf(InterpretationThrottleException);
+    expect(next.handle).not.toHaveBeenCalled();
   });
 });
