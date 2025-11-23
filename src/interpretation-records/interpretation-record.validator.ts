9bdc64c7ad81b84d187497d073ea6cb26b1a15f9
swlee <swo98@naver.com>
2025-11-23 21:32:41 +0900

    add: notfoun validator 추가

---
+++ src/interpretation-records/interpretation-record.validator.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+import { InterpretationRecord } from "./interpretation-record.entity";
+import { InterpretationRecordNotFoundException } from "./exceptions/interpretation-record-not-found.exception";
+
+@Injectable()
+export class InterpretationRecordValidator {
+  public validExists(record?: InterpretationRecord | null) {
+    if (!record) {
+      throw new InterpretationRecordNotFoundException();
+    }
+    return record;
+  }
+}
